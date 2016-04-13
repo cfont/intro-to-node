e31f9b2ba54474c8a7ce35b801828c0068922780
Chris Fontenot <chris.fontenot@gmail.com>
2016-04-13 23:46:38 +0000

    Added new file callback.js
    This file shows a few examples of asynchronous callback functions
    after waiting a random amount of time between calls to show that
    the function calls of evenDoubler do not happen synchronously (in
    order from first to last) or wait until one finishes prior to the
    next one being executed.

---
+++ psitn-1/callback.js
@@ -0,0 +1,34 @@
+var maxTime = 1000;
+
+// If input is even/ double it
+// If input is odd, error
+// (call takes random amount of time < 1s)
+var evenDoubler = function(v,callback) {
+    var waitTime = Math.floor(Math.random()*(maxTime+1));
+    if (v%2) {
+        setTimeout(function(){
+            callback(new Error("Odd Input"));
+        }, waitTime);
+    } else {
+        setTimeout(function() {
+            callback(null, v*2, waitTime);
+        }, waitTime);
+    }
+    
+};
+
+var handleResults = function(err,results,time){
+    if (err){
+        console.log("Error: " + err.message);
+        
+    } else {
+        console.log("The results are: " + results + " (" + time + " ms)");
+        
+    }
+};
+
+evenDoubler(2,handleResults);
+evenDoubler(5,handleResults);
+evenDoubler(10,handleResults);
+
+console.log("-----");
